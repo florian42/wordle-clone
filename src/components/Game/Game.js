d7b33062a2bdde4391768eef87397856aaf93e39
Florian Aumeier <hey@flo.fish>
2023-06-17 15:32:41 +0200

    finish wordle clone
    Signed-off-by: Florian Aumeier <hey@flo.fish>

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import {sample} from "../../utils";
+import {WORDS} from "../../data";
+import GuessInput from "../GuessInput";
+import GuessResults from "../Guesses";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +11,37 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+  const addGuess = (guess) => {
+    setGuesses((guesses) => [...guesses, guess]);
+  };
+
+  const hasWon = guesses.includes(answer);
+  const hasLost = guesses.length >= 5 && !hasWon;
+
+  return (
+    <>
+      <GuessResults guesses={guesses} answer={answer} />
+      {!hasWon && !hasLost && (
+        <GuessInput onSubmit={addGuess} disabled={guesses.length >= 5} />
+      )}
+      {hasWon && (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in{" "}
+            <strong>{guesses.length} guesses</strong>.
+          </p>
+        </div>
+      )}
+      {hasLost && (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Game;
